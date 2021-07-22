a52ace226b24ecdb88ab32784bb2c9da0239ad2b
Fantaly <ragusiemanuel@gmail.com>
2021-07-22 04:07:09 +0200

    Copy file on change

--- script.py
+++ script.py
@@ -2,33 +2,48 @@ from shutil import copyfile
 import ntpath, time, os
 from datetime import datetime
 from datetime import timedelta
+import glob
 
 class Monkey(object):
-    def __init__(this, path):
+    def __init__(this, path, backup, num_max):
         this._cached_stamp = 0
         this.filename = path
+        this.backup = backup
+        this.name = ntpath.basename(this.filename)
+        this.max = num_max 
 
     def ook(this):
         stamp = os.stat(this.filename).st_mtime
         if stamp != this._cached_stamp:
             this._cached_stamp = stamp 
             this.copy()
+            this.remove_old()
 
     def copy(this):
         dateTimeObj = datetime.now()
         start = time.time()
         src = this.filename 
-        dst = 'C:/Users/ragus/Desktop/test/' + dateTimeObj.strftime("%d-%m_%H.%M.%S") + ntpath.basename(this.filename) 
+        dst = this.backup + dateTimeObj.strftime("%d-%m_%H.%M.%S") + this.name
         copyfile(src, dst) 
         end = time.time()
         elapsed = end-start
         print(str(timedelta(seconds=elapsed)))
 
+    def remove_old(this):
+        files = glob.glob(this.backup + "*"+this.name)
+        files.sort(key=os.path.getmtime)
+        print(files)
+        if (len(files) > this.max):
+            for i in range (0,len(files)-this.max):
+                print(files[i])
+                os.remove(files[i])
+            
 
-
-path = 'C:/Users/ragus/Desktop/x64c.rpf'
-object = Monkey(path)
+path = 'C:/Users/ragus/Desktop/test.txt'
+backup = 'C:/Users/ragus/Desktop/test/'
+max = 5
+object = Monkey(path, backup, max)
 
 while(True):
     object.ook()
-
+    
